23c7112d54ab519201f1d8e3efa8e3eb1c4bb102
FinityEx <wojciech.jarosinski12@gmail.com>
2023-04-11 16:39:06 +0200

    updated controller approach

--- src/main/java/com/endtoend/bfit/forms/UserDTO.java
+++ src/main/java/com/endtoend/bfit/forms/UserDTO.java
@@ -3,7 +3,7 @@ package com.endtoend.bfit.forms;
 public class UserDTO {
     private String username;
     private String password;
-    private String newPassword;
+    private final String newPassword;
 
     public UserDTO(String username, String password, String newPassword) {
         this.username = username;
